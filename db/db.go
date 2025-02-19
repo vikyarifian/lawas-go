16a985685f9fc8ac6ca374b05475b9c55fb6540b
vikyarifian <vikyarifiansyah@gmail.com>
2025-02-19 22:56:21 +0700

    balik-db

--- db/db.go
+++ db/db.go
@@ -28,8 +28,8 @@ func ConnectDB() {
 func ConnectDBVercel() {
 
 	// dsn := os.Getenv("DB_USER_VERCEL") + ":" + os.Getenv("DB_PASS_VERCEL") + "@tcp(" + os.Getenv("DB_HOST_VERCEL") + ":" + os.Getenv("DB_PORT_VERCEL") + ")/" + os.Getenv("DB_NAME_VERCEL") + "?charset=utf8mb4&parseTime=True&loc=Local"
-	// dsn := "sql12763547:xsy9uBDeMt@tcp(sql12.freesqldatabase.com:3306)/sql12763547?charset=utf8mb4&parseTime=True&loc=Local"
-	dsn := "mysql_articletoy:489143e292dbb496707d8df847267d1c999e50b8@tcp(ji8k9.h.filess.io:3307)/mysql_articletoy?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "sql12763547:xsy9uBDeMt@tcp(sql12.freesqldatabase.com:3306)/sql12763547?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := "mysql_articletoy:489143e292dbb496707d8df847267d1c999e50b8@tcp(ji8k9.h.filess.io:3307)/mysql_articletoy?charset=utf8mb4&parseTime=True&loc=Local"
 	// println(dsn)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
