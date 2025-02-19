eb6a22fdc25342baf1ef0b70b371001d37126765
Viky Arifiansyah <vikyarifiansyah@gmail.com>
2025-02-19 19:21:10 +0700

    upload-dsn-db

--- db/db.go
+++ db/db.go
@@ -27,14 +27,30 @@ func ConnectDB() {
 
 func ConnectDBVercel() {
 
-	dsn := "sql12763547:xsy9uBDeMt@tcp(sql12.freesqldatabase.com:3306/sql12763547?charset=utf8mb4&parseTime=True&loc=Local"
+	// dsn := os.Getenv("DB_USER_VERCEL") + ":" + os.Getenv("DB_PASS_VERCEL") + "@tcp(" + os.Getenv("DB_HOST_VERCEL") + ":" + os.Getenv("DB_PORT_VERCEL") + ")/" + os.Getenv("DB_NAME_VERCEL") + "?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "sql12763547:xsy9uBDeMt@tcp(sql12.freesqldatabase.com:3306)/sql12763547?charset=utf8mb4&parseTime=True&loc=Local"
+	println(dsn)
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatal("Failed To Connect Database MySQL", err.Error())
+		log.Fatal("Failed To Connect Database MySQL  ("+os.Getenv("DB_HOST_VERCEL")+"/"+os.Getenv("DB_NAME_VERCEL")+")", err.Error())
 		os.Exit(2)
 	}
 
-	log.Println("Connected To MySql Database Successfully")
+	log.Println("Connected To MySql Database Successfully (" + os.Getenv("DB_HOST_VERCEL") + "/" + os.Getenv("DB_NAME_VERCEL") + ")")
 	MySql = db
 
 }
+
+// func ConnectDBVercel() {
+
+// 	dsn := "sql12763547:xsy9uBDeMt@tcp(sql12.freesqldatabase.com:3306/sql12763547?charset=utf8mb4&parseTime=True&loc=Local"
+// 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatal("Failed To Connect Database MySQL", err.Error())
+// 		os.Exit(2)
+// 	}
+
+// 	log.Println("Connected To MySql Database Successfully")
+// 	MySql = db
+
+// }
